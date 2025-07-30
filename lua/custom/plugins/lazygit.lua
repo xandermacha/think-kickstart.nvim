2336e570230e5ad91b1d438d9b055f8927a8df14
xandermacha <michaelbalachandar@gmail.com>
2025-07-30 19:50:57 -0400

    fixed: git config?

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,20 +1,20 @@
 return {
-  'kdheepak/lazygit.nvim',                                                                                   
-  lazy = true,           
-  cmd = {                                                                                        
-    'LazyGit',                                                                                   
-    'LazyGitConfig',                                                                             
-    'LazyGitCurrentFile',                                                                        
-    'LazyGitFilter',                                                                             
+  'kdheepak/lazygit.nvim',
+  lazy = true,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
     'LazyGitFilterCurrentFile',
-  },                                                                                             
-                                                                                                 
-  dependencies = {                                                                               
-    'nvim-lua/plenary.nvim',                                                                     
-  },                                                                                             
-  keys = {                                                                                       
-    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open lazy git' },                                
-    { '<leader>gl', '<cmd>LazyGitLog<cr>', desc = 'Open lazy log' },                             
+  },
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open lazy git' },
+    { '<leader>gl', '<cmd>LazyGitLog<cr>', desc = 'Open lazy log' },
     { '<leader>gf', '<cmd>LazyGitCurrentFile<cr>', desc = 'Open lazy git for curr file' },
-  },                                                                                                                                                                                     
+  },
 }
